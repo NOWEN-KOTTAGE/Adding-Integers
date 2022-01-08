e799924df3107ff211b8d88571582842fa833b6b
KOTTAGE N.V.H <95292564+NOWEN-KOTTAGE@users.noreply.github.com>
2022-01-08 08:17:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+ //Fig. 2.7: Main.java
+//Addition program that inputs two numbers then displays their sum.
+import java.util.Scanner; //program uses class Scanner
+
+public class Main {
+	//main method begins execution of Java application
+public static void main (String[] args) {
+	//create a Scanner to obtain input from the command window
+	Scanner input = new Scanner(System.in);
+	
+	System.out.print("Enter first integer: ");//prompt
+	int number1 = input.nextInt(); //read first number from user
+	
+	System.out.print("Enter second integer: ");//prompt
+	int number2 = input.nextInt(); //read second number from user
+	
+	int sum = number1 + number2; //add numbers, then store total in sum
+	
+	System.out.printf("Sum is %d%n", sum); //display sum
+     }//end method main
+ }//end class Main
